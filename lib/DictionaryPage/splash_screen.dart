ce156053ed50573c843fa2479c0af0ca49608cc6
Harwas <royharwamasatu06@gmail.com>
2024-12-10 16:09:09 +0700

    splash screen
    nambahin splash screen

---
+++ lib/DictionaryPage/splash_screen.dart
@@ -0,0 +1,72 @@
+import 'package:english_dictionary/DictionaryPage/dictionary_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> 
+  with SingleTickerProviderStateMixin {
+  
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 2),() {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const DictionaryHomePage(),
+          ),
+      );
+    });
+  }
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body : Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(255, 5, 234, 255), Color.fromARGB(255, 255, 0, 238)],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children : const [
+            Icon(
+              Icons.edit,
+              size: 80,
+              color: Colors.white,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'English Dictionary',
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
+                fontSize: 32,
+              ),
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
